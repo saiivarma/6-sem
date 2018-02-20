3818d4eca8278552db18b5e8c3a7d7a0a36a709c
Koyalkar Pravin Kishore <pravinkishore.1997@gmail.com>
2018-02-20 10:53:19 +0530

    Update connectionlessIterativeTimeServer.c

--- cnsp/lab/connectionlessIterativeTimeServer.c
+++ cnsp/lab/connectionlessIterativeTimeServer.c
@@ -17,7 +17,7 @@ int main(int argc,char *argv[])
                 printf("\n usage:server<port>");
                 exit(0);
         }
-        if((ls=socket(AF_INET,SOCK_STREAM,0))<0)
+        if((ls=socket(AF_INET,SOCK_DGRAM,0))<0)
         {
                 perror("socket");
                 exit(0);
