b32e5c29d48c38bc1a47bd7a2500363e719c309c
Koyalkar Pravin Kishore <pravinkishore.1997@gmail.com>
2018-03-13 15:20:07 +0530

    Create Main.java

---
+++ cnsp/lab/Main.java
@@ -0,0 +1,24 @@
+package proj;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	try {
+		ServerSocket serverSocket= new ServerSocket(80);
+		boolean isStop=false;
+		while(!isStop){
+			Socket clientSocket=serverSocket.accept();
+			System.out.println("client connected");
+//			System.out.println("client"+clientsocket.getInetAddress().getHostAddress()+"is connected");
+			ClientThread clientthread=new ClientThread(clientSocket);
+			clientthread.start();	
+		}
+	}catch(Exception e) {
+		System.out.println(e.toString());
+	}
+	}
+}
