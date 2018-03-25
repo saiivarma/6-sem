91df68f6777825717e75009cc718a49174e5a379
Koyalkar Pravin Kishore <pravinkishore.1997@gmail.com>
2018-03-26 00:23:01 +0530

    Update SMTPmail.java

--- cnsp/lab/SMTPmail.java
+++ cnsp/lab/SMTPmail.java
@@ -1,12 +1,12 @@
 package test;	
-        import java.util.Properties;
-	import javax.mail.Message;
-	import javax.mail.MessagingException;
-	import javax.mail.PasswordAuthentication;
-	import javax.mail.Session;
-	import javax.mail.Transport;
-	import javax.mail.internet.InternetAddress;
-	import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 public class test
 {
 	   public static void main(String[] args) {
