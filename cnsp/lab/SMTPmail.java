60907177599a6a740bad90129f05d6093d50857d
Koyalkar Pravin Kishore <pravinkishore.1997@gmail.com>
2018-03-26 00:22:14 +0530

    Updated SMTP program indent

--- cnsp/lab/SMTPmail.java
+++ cnsp/lab/SMTPmail.java
@@ -1,5 +1,5 @@
 package test;	
-import java.util.Properties;
+        import java.util.Properties;
 	import javax.mail.Message;
 	import javax.mail.MessagingException;
 	import javax.mail.PasswordAuthentication;
